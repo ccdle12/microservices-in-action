e7905d517dfab778676f7a55a60c20e0048f614d
ccdle12 <chris.coverdale24@gmail.com>
2019-08-08 22:42:10 +0100

    api_gateway/app/routes: adding TODOs

--- api_gateway/app/routes.py
+++ api_gateway/app/routes.py
@@ -6,25 +6,27 @@ from flask import jsonify, request
 import uuid
 import os
 import grpc
-from app import order_service_pb2
-from app import order_service_pb2_grpc
+from app.grpc import order_service_pb2, order_service_pb2_grpc
 
 import sys
+
 sys.path.append('..')
 from logger.log import logger
+
 
 @app.route('/order', methods=['POST'])
 def order():
     print('DEBUG: POST ORDER')
-    data = request.get_json()
-
-    if 'amount' not in data:
+    req = request.get_json()
+    # TODO (ccdle12) use flask Rest Plus to validate.
+    if 'amount' not in req:
         return jsonify({'message': 'amount is missing in request'}), 404
 
-    if 'symbol' not in data:
+    if 'symbol' not in req:
         return jsonify({'message': 'symbol is missing in request'}), 404
 
     try:
+        # Maybe wrap this with a OrderServiceClient?
         channel = grpc.insecure_channel(
             'order_service:{}'.format(os.environ['ORDER_SERVICE_PORT'])
         )
@@ -32,11 +34,10 @@ def order():
         response = stub.CreateOrder(
             order_service_pb2.OrderRequest(
                 user_id="123",
-                symbol=data['symbol'].upper(),
-                amount=data['amount']
+                symbol=req['symbol'].upper(),
+                amount=req['amount']
             )
         )
-        # print('[*] DEBUG')
 
         if 'FAILED' == response.status:
             logger.error('error in order request: {}'.format(str(response.status)))
@@ -47,15 +48,16 @@ def order():
 
         logger.info('response to order request: {}'.format(str(response.status)))
         return jsonify({'message': 'hello'})
+
     except Exception as error:
         logger.error('error in api_gateway, order service is unresponsive')
         return jsonify({'message': 'order_service unavailable'}), 500
-        # return jsonify({'message': str(error)}), 500
 
 @app.route('/order', methods=['GET'])
 def all_orders():
     # Utils?
     print('DEBUG: GET ALL ORDERS')
+    # TODO (ccdle12): Marshmallow?
     def deserialize_orders(order):
         order_dict = {}
         order_dict['order_id'] = order.order_id
