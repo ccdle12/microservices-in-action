9f9d3f3f2ac5fee7836bc86155b070551f9c2465
ccdle12 <chris.coverdale24@gmail.com>
2019-08-13 17:55:55 +0100

    api_gateway/config: removing unused PORT variable

--- api_gateway/config.py
+++ api_gateway/config.py
@@ -6,7 +6,6 @@ class Config():
     ENV = 'production'
     DEBUG = False
     TESTING = False
-    PORT = os.environ['ORDER_SERVICE_PORT']
 
 class DevelopmentConfig(Config):
     ENV = 'development'
