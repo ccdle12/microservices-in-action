2a3b001e81a6bdc8b8a9bb677cc986be8689d0a4
ccdle12 <chris.coverdale24@gmail.com>
2019-10-17 22:08:46 +0100

    orders_service/main: refactoring to use eventqueue

--- orders_service/main.go
+++ orders_service/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"github.com/simplebank/orders_service/gatewayserver"
+	"github.com/simplebank/orders_service/eventqueue"
+	"github.com/simplebank/orders_service/gatewaygrpc"
 	"log"
 	"time"
 )
 
 func main() {
-	// Creates a grpc server for the api_gateway to send requests.
-	srv, err := gatewayserver.MakeAPIGatewayServer()
+	// Creates a connection to the order event queue.
+	orderEventQueue, err := eventqueue.New()
+	if err != nil {
+		log.Fatalf("Failed to create queue client")
+	}
+	defer orderEventQueue.Cleanup()
+
+	// Creates a grpc server, allowing the api_gateway to send requests to the
+	// order service.
+	apiGatewayServer, err := gatewaygrpc.New(orderEventQueue)
 	if err != nil {
 		log.Fatalf("Failed to create server for the APIGateway: %v", err)
 	}
-	defer srv.Serve()
+	defer apiGatewayServer.Serve() // is blocking.
 
-	// TEMP
-	// Will be used for consuming channels.
+	// Listen to messages from the Event Queue.
 	go func() {
 		for {
 			select {
+			case <-orderEventQueue.CloseChann:
+				log.Println("closing...")
 			default:
 				log.Println("blocking...")
 				time.Sleep(5 * time.Second)
