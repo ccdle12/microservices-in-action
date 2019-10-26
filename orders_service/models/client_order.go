d0bea42e4128952864a971521f99d2ca0e062ccd
ccdle12 <chris.coverdale24@gmail.com>
2019-10-26 14:47:00 +0100

    orders_service/models/client_order: updating UUID and removing gorm

--- orders_service/models/client_order.go
+++ orders_service/models/client_order.go
@@ -1,12 +1,12 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	"github.com/google/uuid"
 )
 
+// ClientOrder represents a clients request for a trade order.
 type ClientOrder struct {
-	gorm.Model
-	Id        string
+	Id        uuid.UUID
 	Symbol    string
 	OrderSize string
 	Price     string
