8d8609dae7867e7ef769140aef244e89d8cf4f0d
ccdle12 <chris.coverdale24@gmail.com>
2019-10-26 14:47:15 +0100

    orders_service/models/client_order_test: updating UUID

--- orders_service/models/client_order_test.go
+++ orders_service/models/client_order_test.go
@@ -1,13 +1,19 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"testing"
 )
 
 // Test that a client order can be created.
 func TestClientOrder(t *testing.T) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		t.Errorf("Failed to generate uuid: %v", err)
+	}
+
 	_ = &ClientOrder{
-		Id:        "123",
+		Id:        uuid,
 		Symbol:    "BTC",
 		OrderSize: "1234",
 		Price:     "123",
